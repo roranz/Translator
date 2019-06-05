f07532e300799039d72c887636e0a534c466d961
roranz <45340612+roranz@users.noreply.github.com>
2019-06-05 22:15:44 +0200

    Add files via upload

---
+++ TraduttoreForms/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace TraduttoreForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.apriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.traduciToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inniettaTraduzioneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.apiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Subtitle = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            this.saveFileDialog1.Title = "Salva";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Title = "Apri";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.DetectUrls = false;
+            this.richTextBox1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 116);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(500, 532);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.apriToolStripMenuItem,
+            this.traduciToolStripMenuItem,
+            this.salvaToolStripMenuItem,
+            this.inniettaTraduzioneToolStripMenuItem,
+            this.apiToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1034, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // apriToolStripMenuItem
+            // 
+            this.apriToolStripMenuItem.Name = "apriToolStripMenuItem";
+            this.apriToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.apriToolStripMenuItem.Text = "Apri";
+            this.apriToolStripMenuItem.Click += new System.EventHandler(this.apriToolStripMenuItem_Click);
+            // 
+            // traduciToolStripMenuItem
+            // 
+            this.traduciToolStripMenuItem.Name = "traduciToolStripMenuItem";
+            this.traduciToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.traduciToolStripMenuItem.Text = "Traduci";
+            this.traduciToolStripMenuItem.Click += new System.EventHandler(this.traduciToolStripMenuItem_Click);
+            // 
+            // salvaToolStripMenuItem
+            // 
+            this.salvaToolStripMenuItem.Name = "salvaToolStripMenuItem";
+            this.salvaToolStripMenuItem.Size = new System.Drawing.Size(107, 20);
+            this.salvaToolStripMenuItem.Text = "Salva Traduzione";
+            this.salvaToolStripMenuItem.Click += new System.EventHandler(this.salvaToolStripMenuItem_Click);
+            // 
+            // inniettaTraduzioneToolStripMenuItem
+            // 
+            this.inniettaTraduzioneToolStripMenuItem.Name = "inniettaTraduzioneToolStripMenuItem";
+            this.inniettaTraduzioneToolStripMenuItem.Size = new System.Drawing.Size(120, 20);
+            this.inniettaTraduzioneToolStripMenuItem.Text = "Innietta Traduzione";
+            this.inniettaTraduzioneToolStripMenuItem.Click += new System.EventHandler(this.inniettaTraduzioneToolStripMenuItem_Click);
+            // 
+            // apiToolStripMenuItem
+            // 
+            this.apiToolStripMenuItem.Name = "apiToolStripMenuItem";
+            this.apiToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.apiToolStripMenuItem.Text = "Api";
+            this.apiToolStripMenuItem.Click += new System.EventHandler(this.apiToolStripMenuItem_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.DetectUrls = false;
+            this.richTextBox2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(522, 116);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(500, 532);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // saveFileDialog2
+            // 
+            this.saveFileDialog2.Filter = "ASS files (*.ass)|*.ASS|All files (*.*)|*.*";
+            this.saveFileDialog2.Title = "Salva";
+            this.saveFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog2_FileOk);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(291, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Subtitle
+            // 
+            this.Subtitle.AutoSize = true;
+            this.Subtitle.Font = new System.Drawing.Font("Oswald", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Subtitle.Location = new System.Drawing.Point(377, 34);
+            this.Subtitle.Name = "Subtitle";
+            this.Subtitle.Size = new System.Drawing.Size(419, 71);
+            this.Subtitle.TabIndex = 5;
+            this.Subtitle.Text = "Subtitles Translator";
+            this.Subtitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1034, 681);
+            this.Controls.Add(this.Subtitle);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Traduttore by GreenCoffeeSubs v0.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem apriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem traduciToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inniettaTraduzioneToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.ToolStripMenuItem apiToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Subtitle;
+    }
+}
+
---
+++ TraduttoreForms/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+using System.Collections.Generic;
+
+namespace TraduttoreForms
+{
+    public partial class Form1 : Form
+    {
+        String api = "";
+        bool tradotto = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            StreamWriter myStream = File.CreateText(saveFileDialog1.FileName);
+            myStream.Write(richTextBox2.Text);
+            myStream.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            openFileDialog1.OpenFile();
+            var filename = openFileDialog1.FileName;
+            var fileStream = openFileDialog1.OpenFile();
+
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                StreamReader fileRead = File.OpenText(openFileDialog1.FileName);
+                richTextBox1.Text = Estrattore(fileRead);
+                fileRead.Close();
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aaaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void apriToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            tradotto = false;
+        }
+
+        private void salvaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Inseritore(StreamReader file)
+        {
+            var esiste = false;
+            if (File.Exists("traduzione.txt"))
+                esiste = true;
+            char ch;
+            StreamWriter myStream = File.CreateText(@"traduzione.txt");
+            myStream.Write(richTextBox2.Text);
+            myStream.Close();
+            StreamReader fileTrad = File.OpenText(@"traduzione.txt");
+
+            saveFileDialog2.ShowDialog();
+            StreamWriter str = File.CreateText(saveFileDialog2.FileName);
+            while (!file.EndOfStream)
+            {
+                ch = (char)file.Read();
+                str.Write(ch);
+                if (ch == '0')
+                {
+                    ch = (char)file.Read();
+                    str.Write(ch);
+                    if (ch == ',')
+                    {
+                        ch = (char)file.Read();
+                        str.Write(ch);
+                        if (ch == ',')
+                        {
+                            while (ch != '\n')
+                            {
+                                ch = (char)fileTrad.Read();
+                                if (ch == ' ')
+                                {
+                                    ch = (char)fileTrad.Read();
+                                    if (ch != '\\')
+                                        str.Write(' ');
+                                    else
+                                    {
+                                        str.Write(ch);
+                                        ch = (char)fileTrad.Read();
+                                        str.Write(ch);
+                                        ch = (char)fileTrad.Read();
+                                        ch = (char)fileTrad.Read();
+                                    }
+                                }
+                                str.Write(ch);
+                            }
+                            file.ReadLine();
+                        }
+                    }
+                }
+            }
+            if (!esiste)
+                File.Delete("traduzione.txt");
+            file.Close();
+            fileTrad.Close();
+            str.Close();
+        }
+
+        private string Estrattore(StreamReader file)
+        {
+            string str = "";
+            char ch;
+            while (!file.EndOfStream)
+            {
+                ch = (char)file.Read();
+                if (ch == '0')
+                {
+                    ch = (char)file.Read();
+                    if (ch == ',')
+                    {
+                        ch = (char)file.Read();
+                        if (ch == ',')
+                        {
+                            for (; ch != '\n';)
+                            {
+                                ch = (char)file.Read();
+                                if (ch == '{')
+                                {
+                                    while (!(ch == '}'))
+                                    {
+                                        ch = (char)file.Read();
+                                    }
+                                }
+                                else if (ch == '\\')
+                                {
+                                    str += " " + ch;
+                                    ch = (char)file.Read();
+                                    if (ch == 'N' || ch == 'n')
+                                    {
+                                        str += ch + " ";
+                                    }
+                                }
+                                else
+                                {
+                                    str += ch;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return str + "\n";
+        }
+
+        internal class Messaggio
+        {
+            public short code { get; set; }
+            public string lang { get; set; }
+            public string[] text { get; set; }
+            public string getText()
+            {
+                string str = "";
+                for (int i = 0; i < text.Length; ++i)
+                {
+                    str += text[i];
+                }
+                return str;
+            }
+        }
+        public void traduciToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tradotto = false;
+            if(api == "")
+            {
+                api = InputBox("Inserisci l'api da https://translate.yandex.com/developers/keys", "API");
+            }
+            if (richTextBox2.Text == "" && api != "")
+            {
+                string encodedString = System.Net.WebUtility.UrlEncode(richTextBox1.Text);
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                var trad = "";
+                string[] result = contaCaratteri(encodedString,9000);
+                try
+                {
+                    for(int i = 0; i < result.Length; ++i)
+                    {
+                        if (result[i].Length > 3)
+                        {
+                        MessageBox.Show("owo");
+                        string link = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=" + api + "&text=" + result[i] + "&lang=it";
+                        string response = webClient.DownloadString(link);
+                        trad += JsonConvert.DeserializeObject<Messaggio>(response).getText();
+                        }
+                    }
+                    tradotto = true;
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.ToString());
+                    return;
+                }
+                richTextBox2.Text = trad;
+                tradotto = true;
+
+
+            }
+            else
+            {
+                if (api != "")
+                    MessageBox.Show("La textbox della traduzione dev'essere vuota!", "Errore");
+                else
+                    MessageBox.Show("API non valido", "Errore");
+            }
+        }
+
+        private string[] contaCaratteri(string s,int car)
+        {
+            string[] stringhe = s.Split(new string[] { "%0A" }, StringSplitOptions.RemoveEmptyEntries);
+            string[] owo = new string[5]{"", "", "", "", ""};
+            int count = 0;
+            int nStr = 0;
+            for (int i = 0; i < stringhe.Length; ++i)
+            {
+                count += stringhe[i].Length;
+
+                if(count >= car)
+                {
+                    count = 0;
+                    nStr++;
+                }                   
+                owo[nStr] += stringhe[i] += "%0A";                
+            }
+                return owo;
+        }
+
+        private void apiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            api = InputBox("Inserisci l'api da https://translate.yandex.com/developers/keys", "API");
+        }
+
+        public static string InputBox(string text, string caption)
+        {
+            Form prompt = new Form()
+            {
+                Width = 400,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            Label textLabel = new Label() { Left = 50, Top = 20, Width = 320, Text = text };
+            TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 300 };
+            Button confirmation = new Button() { Text = "Ok", Left = 250, Width = 100, Top = 80, DialogResult = DialogResult.OK };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = confirmation;
+
+            return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+        }
+
+        private void saveFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void inniettaTraduzioneToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StreamReader file;
+            if (!tradotto)
+                return;
+            if (richTextBox1.Text.Length > 5)
+            {
+                Inseritore(file = new StreamReader(openFileDialog1.FileName));
+                file.Close();
+            }
+            else MessageBox.Show("Non ho niente il file di origine", "Errore!");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
